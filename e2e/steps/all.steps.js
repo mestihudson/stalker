b163f274895ee36e38bfc845481667f503615d5f
mestihudson <mestihudson@gmail.com>
2020-09-08 18:27:37 -0300

    chore(env): add create-task/save-task scenario

---
+++ e2e/steps/all.steps.js
@@ -0,0 +1,13 @@
+const { Given, When, Then } = require('cucumber')
+
+Given(`User fills required info`, () => {
+  return `pending`
+})
+
+When(`User saves the task`, () => {
+  return `pending`
+})
+
+Then(`Task has been added`, () => {
+  return `pending`
+})
