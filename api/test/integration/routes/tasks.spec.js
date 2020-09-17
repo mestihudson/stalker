94a6c18d02fcaf1811eeb03a3c191ccd1fdfa5f5
mestihudson <mestihudson@gmail.com>
2020-09-17 11:31:35 -0300

    fix: add assertion to return of test

--- api/test/integration/routes/tasks.spec.js
+++ api/test/integration/routes/tasks.spec.js
@@ -5,9 +5,11 @@ import app from '@/server'
 describe(`tasks`, () => {
   describe(`post`, () => {
     it(`should return a 201 http code when successful creation`, async () => {
-      await request(app).post(`/api/tasks`)
+      await request(app)
+        .post(`/api/tasks`)
         .send({ name: 'Name of Task' })
         .expect(201)
+        .expect({})
     })
   })
 })
