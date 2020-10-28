26fb6e0cfe1638601860453f97aa50025af0cddb
mestihudson <mestihudson@gmail.com>
2020-10-27 22:24:23 -0300

    test(e2e): add support code

---
+++ e2e/support/Application.js
@@ -0,0 +1,38 @@
+const { By, Builder } = require('selenium-webdriver')
+const { expect } = require('chai')
+
+class Application {
+  constructor() {
+    this.driver = new Builder()
+      .forBrowser("chrome")
+      .usingServer("http://hub:4444/wd/hub")
+      .build()
+  }
+
+  async open() {
+    await this.driver.get("http://app:3000")
+  }
+
+  async fillTask() {
+    const input = await this.driver.findElement(
+      By.xpath(`//*[@data-input='Name']`)
+    )
+    await input.sendKeys("Task name")
+  }
+
+  async saveTask() {
+    const trigger = await this.driver.findElement(
+      By.xpath(`//*[@data-trigger='Save']`)
+    )
+    await trigger.click()
+  }
+
+  async hasShowedSuccessfulMessage() {
+    const components = await this.driver.findElements(
+      By.xpath(`//*[@data-component='Alert']`)
+    )
+    expect(components.length).to.equal(1)
+  }
+}
+
+module.exports = Application
