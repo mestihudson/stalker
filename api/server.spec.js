5c720e7e45ab3b57b8f50832a2d853b3b2368124
mestihudson <mestihudson@gmail.com>
2020-10-29 18:54:26 -0300

    refactor(test/api/integration/rest): remove duplication

--- api/server.spec.js
+++ api/server.spec.js
@@ -12,21 +12,26 @@ describe(`server`, () => {
   describe(`/api/tasks`, () => {
     describe(`post`, () => {
       describe(`when successful`, () => {
-        it(`should return 201 http code`, async () => {
+        const endpoint = "/api/tasks"
+
+        const task = { name: 'Name of Task' }
+
+        const apiCreateTask = async ({ assertion }) => {
           await request(server)
-            .post(`/api/tasks`)
-            .send({ name: 'Name of Task' })
-            .expect((response) => expect(response.status).toBe(201))
+            .post(endpoint)
+            .send(task)
+            .expect(assertion)
+        }
+
+        it(`should return 201 http code`, async () => {
+          const assertion = (response) => expect(response.status).toBe(201)
+          await apiCreateTask({ assertion })
         })
 
         it(`should call usecase with received task`, async () => {
-          const task = { name: 'Name of Task' }
-          await request(server)
-            .post(`/api/tasks`)
-            .send(task)
-            .expect((response) => {
-              expect(createTask).toHaveBeenCalledWith(task)
-            })
+          const assertion = (response) => expect(createTask)
+            .toHaveBeenCalledWith(task)
+          await apiCreateTask({ assertion })
         })
       })
     })
