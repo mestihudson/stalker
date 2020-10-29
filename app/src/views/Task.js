a2a62367715a731a9d4af4f4af3b244413f87fd1
mestihudson <mestihudson@gmail.com>
2020-10-29 00:21:22 -0300

    feat(app): should show successful message when api has callbacked

--- app/src/views/Task.js
+++ app/src/views/Task.js
@@ -4,6 +4,8 @@ import Api from '../services/Api'
 
 function Task() {
   const [name, setName] = useState('')
+  const [showMessage, setShowMessage] = useState(false)
+  const [message, setMessage] = useState('')
 
   const handleNameChange = ({ target }) => {
     setName(target.value)
@@ -11,13 +13,17 @@ function Task() {
 
   const handleSaveClick = () => {
     Api.createTask({ name })
-      .then(() => console.log())
+      .then(() => {
+        setShowMessage(true)
+        setMessage(`Task successful created.`)
+      })
   }
 
   return (
     <>
       <input type="text" data-input="Name" onChange={handleNameChange}/>
       <button data-trigger="Save" onClick={handleSaveClick}>Save</button>
+      { showMessage && <span data-component="Alert">{ message }</span> }
     </>
   )
 }
