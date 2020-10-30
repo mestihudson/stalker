ea98d9b3121a35d06c5913abbe4316a3d5bff498
mestihudson <mestihudson@gmail.com>
2020-10-30 10:16:16 -0300

    refactor(test/api/integration/repository): remove duplication

--- api/taskRepository.spec.js
+++ api/taskRepository.spec.js
@@ -1,26 +1,16 @@
-const utils = require('./utils')
+const Backdoor = require('/ws/e2e/support/Backdoor')
 
 const repository = require('./taskRepository')
 
 describe(`taskRepository`, () => {
   describe(`saveTask`, () => {
-    const directory = '/ws/.backdoor'
-    const file = 'tasks.json'
-
-    const ensureThereIsNoTask = async () => await utils
-      .writeTo([], directory, file)
-
-    const thereIsOneTask = async () => {
-      const tasks = await utils.readFrom(directory, file)
-      expect(tasks).toHaveLength(1)
-    }
-
     it(`should persist received task getting id for it`, async () => {
-      await ensureThereIsNoTask()
+      const backdoor = new Backdoor()
+      await backdoor.ensureThereIsNoTask()
       const task = { name: 'Name of Task to Persist' }
       const result = await repository.saveTask(task)
       expect(result).toMatchObject({ id: expect.any(Number) })
-      await thereIsOneTask()
+      await backdoor.thereIsOneTask()
     })
   })
 })
