63dab116b77531c397d979c0a5829dd4fb050e6e
mestihudson <mestihudson@gmail.com>
2020-10-29 23:52:21 -0300

    feat(app/integration): add axios to api service

--- app/src/services/Api.js
+++ app/src/services/Api.js
@@ -1,6 +1,9 @@
+import axios from 'axios'
+
 const Api = {
-  createTask () {
-    return Promise.resolve()
+  createTask (task) {
+    return axios.post(`/tasks`, task)
+      .then(({ data }) => Promise.resolve(data))
   }
 }
 
