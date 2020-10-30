40e3b9f84749bcfa4077e2d45cc3eb3b4ddd9128
mestihudson <mestihudson@gmail.com>
2020-10-30 19:01:31 -0300

    add: "save task" cucumber scenario steps

---
+++ e2e/steps/all.steps.js
@@ -0,0 +1,13 @@
+const { Given, When, Then } = require('cucumber')
+
+Given(`User fills required info`, async () => {
+  return 'pending'
+})
+
+When(`User saves the task`, async () => {
+  return 'pending'
+})
+
+Then(`Task has been added`, async () => {
+  return 'pending'
+})
