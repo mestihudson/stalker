c492c7b0436694d378b06f57c560d772bd2a6886
mestihudson <mestihudson@gmail.com>
2020-10-29 23:21:47 -0300

    test(api/integration/repository): should persist received task getting id for it from taskRepository.saveTask

---
+++ api/taskRepository.spec.js
@@ -0,0 +1,26 @@
+const utils = require('./utils')
+
+const repository = require('./taskRepository')
+
+describe(`taskRepository`, () => {
+  describe(`saveTask`, () => {
+    const directory = '/ws/.backdoor'
+    const file = 'task.json'
+
+    const ensureThereIsNoTask = async () => await utils
+      .writeTo([], directory, file)
+
+    const thereIsOneTask = async () => {
+      const tasks = await utils.readFrom(directory, file)
+      expect(tasks).toHaveLength(1)
+    }
+
+    it(`should persist received task getting id for it`, async () => {
+      await ensureThereIsNoTask()
+      const task = { name: 'Name of Task to Persist' }
+      const result = await repository.saveTask(task)
+      expect(result).toMatchObject({ id: expect.any(Number) })
+      await thereIsOneTask()
+    })
+  })
+})
